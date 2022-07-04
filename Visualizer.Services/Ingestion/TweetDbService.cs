4805a8bbb8533bc411fc6908a1c3b7dcc13c1e1c
Marius Muntean <marius.muntean@live.com>
2022-07-04 22:41:14 +0200

    Using ILogger

--- Visualizer.Services/Ingestion/TweetDbService.cs
+++ Visualizer.Services/Ingestion/TweetDbService.cs
@@ -1,20 +1,23 @@
 using Mapster;
+using Microsoft.Extensions.Logging;
 using Redis.OM;
 using Tweetinvi.Events.V2;
-using Visualizer.Model;
+using Visualizer.Model.TweetDb;
 
 namespace Visualizer.Services.Ingestion;
 
 public class TweetDbService
 {
     private readonly RedisConnectionProvider _redisConnectionProvider;
+    private readonly ILogger<TweetDbService> _logger;
 
-    public TweetDbService(RedisConnectionProvider redisConnectionProvider)
+    public TweetDbService(RedisConnectionProvider redisConnectionProvider, ILogger<TweetDbService> logger)
     {
         _redisConnectionProvider = redisConnectionProvider;
+        _logger = logger;
     }
 
-    public async Task<string> Store(TweetV2ReceivedEventArgs tweetV2ReceivedEventArgs)
+    public async Task Store(TweetV2ReceivedEventArgs tweetV2ReceivedEventArgs)
     {
         string internalId;
         try
@@ -22,13 +25,11 @@ public class TweetDbService
             var tweetCollection = _redisConnectionProvider.RedisCollection<TweetModel>();
             var tweetModel = tweetV2ReceivedEventArgs.Tweet.Adapt<TweetModel>();
             internalId = await tweetCollection.InsertAsync(tweetModel);
+            _logger.LogInformation(internalId);
         }
         catch (Exception e)
         {
-            await Console.Error.WriteLineAsync($"Failed to process tweet event {tweetV2ReceivedEventArgs?.Tweet?.Id} {e.Message} {e.StackTrace}");
-            return null;
+            _logger.LogError(e, "Failed to process tweet event {Id}", tweetV2ReceivedEventArgs?.Tweet?.Id);
         }
-
-        return internalId;
     }
 }
